47bc1cdd941dc1bc84f1c506bd6826239aa7f35c
Loris Sauter <loris.sauter@unibas.ch>
2019-04-12 18:10:35 +0200

    UV mapping for tiled cubes

--- Assets/Scripts/Unibas/DBIS/VREP/Puzzle/PuzzleCubeFactory.cs
+++ Assets/Scripts/Unibas/DBIS/VREP/Puzzle/PuzzleCubeFactory.cs
@@ -3,43 +3,99 @@ using UnityEngine;
 
 namespace Unibas.DBIS.VREP.Puzzle {
   
-  
   public class PuzzleCubeFactory {
+    
+    
+    
+    public static Vector2[] CreateTiledUVMap(float width, float height, int k = 3) {
+      float r = width / height;
+      float u = width / r;
+      float v = height / r;
 
-    /// <summary>
-    /// Crops an image from its center into a square image.
-    /// </summary>
-    /// <param name="image"></param>
-    /// <returns></returns>
-    public static Vector2[] CropCenteredCoordinates(Texture image) {
-      float w2 = image.width / 2f;
-      float h2 = image.height / 2f;
-      float s2 = Math.Min(image.width, image.width) / 2f;
-      return new[] {
-        new Vector2(w2-s2, h2-s2), new Vector2(w2+s2, h2+s2)
-      };
+      Vector2[] arr = new Vector2[(k + 1) * (k + 1)];
+
+      for (int y = 0; y <= k; y++) {
+        for (int x = 0; x <= k; x++) {
+          arr[k * y + x] = new Vector2(x * u, y * v);
+        }
+      }
+
+      return arr;
     }
     
-    public static Vector2[] TileImage(Texture2D image) {
-      float w2 = image.width / 2f;
-      float h2 = image.height / 2f;
-      float s2 = Math.Min(image.width, image.width) / 2f;
+    
 
-      Vector2 tile0LowerLeft = new Vector2(w2 - s2, h2 - s2);
-      Vector2 tile0UpperRight = new Vector2(w2, h2);
-      Vector2 tile1LowerLeft = new Vector2(w2, h2 - s2);
-      Vector2 tile1UpperRight = new Vector2(w2 + s2, h2);
-      Vector2 tile2LowerLeft = new Vector2(w2, h2);
-      Vector2 tile2UpperRight = new Vector2(w2 + s2, h2 + s2);
-      Vector2 tile3LowerLeft = new Vector2(w2 - s2, h2);
-      Vector2 tile3UpperRight = new Vector2(w2, h2);
+    public static GameObject CreatePlane(float width, float height, Material mat = null) {
+      GameObject go = new GameObject("Plane");
+      MeshFilter meshFilter = go.AddComponent<MeshFilter>();
+      MeshRenderer meshRenderer = go.AddComponent<MeshRenderer>();
+      Mesh mesh = meshFilter.mesh;
+      Vector3[] vertices = new Vector3[4];
+      vertices[0] = new Vector3(0, 0, 0);
+      vertices[1] = new Vector3(width, 0, 0);
+      vertices[2] = new Vector3(0, height, 0);
+      vertices[3] = new Vector3(width, height, 0);
 
-      return new[] {
-        tile0LowerLeft, tile0UpperRight,
-        tile1LowerLeft, tile1UpperRight,
-        tile2LowerLeft, tile2UpperRight,
-        tile3LowerLeft, tile3UpperRight
-      };
+      mesh.vertices = vertices;
+
+      int[] tri = new int[6];
+
+      tri[0] = 0;
+      tri[1] = 2;
+      tri[2] = 1;
+
+      tri[3] = 2;
+      tri[4] = 3;
+      tri[5] = 1;
+
+      mesh.triangles = tri;
+
+      Vector3[] normals = new Vector3[4];
+
+      normals[0] = -Vector3.forward;
+      normals[1] = -Vector3.forward;
+      normals[2] = -Vector3.forward;
+      normals[3] = -Vector3.forward;
+
+      mesh.normals = normals;
+
+      Vector2[] uv = new Vector2[4];
+
+      float xUnit = 1;
+      float yUnit = 1;
+
+      if (width > height) {
+        xUnit = width / height;
+      } else {
+        yUnit = height / width;
+      }
+
+      uv[0] = new Vector2(0, 0);
+      uv[1] = new Vector2(xUnit, 0);
+      uv[2] = new Vector2(0, yUnit);
+      uv[3] = new Vector2(xUnit, yUnit);
+
+      mesh.uv = uv;
+
+      mesh.RecalculateBounds();
+      mesh.RecalculateNormals();
+      mesh.RecalculateTangents();
+
+      if (mat != null) {
+        meshRenderer.material.CopyPropertiesFromMaterial(mat);
+        //meshRenderer.material.SetTextureScale("_MainTex", new Vector2(1,1));
+        meshRenderer.material.name = mat.name;
+      } else {
+        meshRenderer.material = new Material(Shader.Find("Standard"));
+        meshRenderer.material.color = Color.white;
+      }
+
+      // TODO Highly experimental!
+
+      var boxCollider = go.AddComponent<BoxCollider>();
+      boxCollider.size = new Vector3(width, height, 0.0001f);
+
+      return go;
     }
   }
 }
