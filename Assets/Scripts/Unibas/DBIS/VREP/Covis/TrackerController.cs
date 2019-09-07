7c2accaa194e0eb3e65b2e34a172dc7afef1eca6
loris.sauter <loris.sauter@unibas.ch>
2019-09-07 15:57:52 +0200

    Implemented basic vive tracker management.

---
+++ Assets/Scripts/Unibas/DBIS/VREP/Covis/TrackerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class TrackerController : MonoBehaviour
+{
+    public GameObject trackerPrefab;
+    
+    void OnEnable() {
+        SteamVR_Events.DeviceConnected.Listen(OnDeviceConnected);
+        Debug.Log("Listening for connected devices...");
+    }
+ 
+    private void OnDeviceConnected(int index, bool connected) {
+        if(connected) {
+            if(OpenVR.System != null) {
+                ETrackedDeviceClass deviceClass = OpenVR.System.GetTrackedDeviceClass((uint) index);
+                if(deviceClass == ETrackedDeviceClass.GenericTracker) {
+                    Debug.Log("Vive Tracker was connected at index: " + index);
+                    GameObject tracker = Instantiate(trackerPrefab);
+                    tracker.GetComponent<SteamVR_TrackedObject>().index = (SteamVR_TrackedObject.EIndex)index;
+                    tracker.transform.SetParent(transform);
+                }
+            }
+        }
+    }
+}
